4e65de56d3277db52c0e8feccfddc79185b8b8b7
Atom <atom@atom.com>
2018-03-19 16:49:32 +0800

    add proxy generate

---
+++ spring-source/src/test/java/com/watent/source/cglib/GeneratorUtil.java
@@ -0,0 +1,40 @@
+package com.watent.source.cglib;
+
+import org.junit.Test;
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 生成代理类
+ *
+ * @author Dylan
+ * @date 2018/3/19 16:24
+ */
+public class GeneratorUtil {
+
+
+    @Test
+    public void generate() {
+        byte[] data = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{Hello.class});
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("$Proxy0.class");
+            fos.write(data);
+            System.out.println(" generate ok ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ spring-source/src/test/java/com/watent/source/cglib/Hello.java
@@ -0,0 +1,14 @@
+package com.watent.source.cglib;
+
+/**
+ * 测试
+ *
+ * @author Dylan
+ * @date 2018/3/19 16:25
+ */
+public interface Hello {
+
+    void haha();
+
+    void xixi();
+}
